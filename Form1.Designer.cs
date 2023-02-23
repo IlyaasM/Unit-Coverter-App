28ea9d586be44f14304a7539a8b6cb105c754ded
IlyaasM <121197052+IlyaasM@users.noreply.github.com>
2023-02-22 20:23:51 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace PRG455Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Silver;
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(179, 259);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Units";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(46, 195);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(102, 19);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "TEMPERATURE";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(46, 156);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(51, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "TIME";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(46, 105);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "MASS";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(46, 60);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "LENGTH";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(399, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(339, 124);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(193, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(38, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "FROM";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(399, 189);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(339, 124);
+            this.panel2.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(193, 56);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(114, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(38, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "TO";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(500, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(528, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Unit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(768, 392);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Unit Convertor                                                                   " +
+    "                      LAB2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG455Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double userin= 0.0, userout=0.0;
+       
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            string[] length = new string[] { "Centimeter", "Meter", "Kilometer"};
+            if (radioButton1.Checked == true)
+                {
+               
+                foreach (string item in length)
+                {
+                    comboBox1.Items.Add(item);
+                    comboBox2.Items.Add(item);
+                }
+                 label1.Text = "Convert Length";
+                 textBox1.Text = "";
+                 textBox1.Focus();
+            }
+                else
+                {
+                label1.Text = "Select Unit";
+                textBox1.Text = "";
+                textBox2.Text = "";
+                comboBox1.Text="";
+                foreach (string item in length)
+                {
+                    comboBox1.Items.Remove(item);
+                    comboBox2.Items.Remove(item);
+                }
+            }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2_SelectedIndexChanged(sender, e);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            string[] Mass = new string[] { "Milligram", "Gram", "Kilogram" };
+            if (radioButton2.Checked == true)
+            {
+
+                foreach (string item in Mass)
+                {
+                    comboBox1.Items.Add(item);
+                    comboBox2.Items.Add(item);
+                }
+                label1.Text = "Convert Mass";
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+            else
+            {
+                label1.Text = "Select Unit";
+                textBox1.Text = "";
+                textBox2.Text = "";
+                comboBox1.Text = "";
+                comboBox2.Text = "";
+                foreach (string item in Mass)
+                {
+                    comboBox1.Items.Remove(item);
+                    comboBox2.Items.Remove(item);
+                }
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            string[] Time = new string[] { "Seconds", "Minutes", "Hours" };
+            if (radioButton3.Checked == true)
+            {
+
+                foreach (string item in Time)
+                {
+                    comboBox1.Items.Add(item);
+                    comboBox2.Items.Add(item);
+                }
+                label1.Text = "Convert Time";
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+            else
+            {
+                label1.Text = "Select Unit";
+                textBox1.Text = "";
+                textBox2.Text = "";
+                comboBox1.Text = "";
+                comboBox2.Text = "";
+                foreach (string item in Time)
+                {
+                    comboBox1.Items.Remove(item);
+                    comboBox2.Items.Remove(item);
+                }
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            string[] Temp = new string[] { "Celsius", "Fahrenheit", "Kelvins" };
+            if (radioButton4.Checked == true)
+            {
+
+                foreach (string item in Temp)
+                {
+                    comboBox1.Items.Add(item);
+                    comboBox2.Items.Add(item);
+                }
+                label1.Text = "Convert Temperatures";
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+            else
+            {
+                label1.Text = "Select Unit";
+                textBox1.Text = "";
+                textBox2.Text = "";
+                comboBox1.Text = "";
+                comboBox2.Text = "";
+                foreach (string item in Temp)
+                {
+                    comboBox1.Items.Remove(item);
+                    comboBox2.Items.Remove(item);
+                }
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //LENGTH CONVERSIONS
+            if (comboBox1.Text == comboBox2.Text)
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin;
+                textBox2.Text = Convert.ToString(userout);
+                MessageBox.Show("No Conversion Needed Here...\nSelect Another Unit to Convert to","Incorrect");
+            }
+            if (comboBox1.Text=="Centimeter"&& comboBox2.Text == "Meter")
+            {
+               userin=Convert.ToDouble(textBox1.Text);
+                userout = userin / 100;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Centimeter" && comboBox2.Text == "Kilometer")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin / 100000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Meter" && comboBox2.Text == "Centimeter")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin * 100;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Meter" && comboBox2.Text == "Kilometer")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin / 1000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Kilometer" && comboBox2.Text == "Centimeter")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin * 100000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Kilometer" && comboBox2.Text == "Meter")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin * 1000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            //MASS CONVESR\IONS
+            if (comboBox1.Text == "Milligram" && comboBox2.Text == "Gram")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin / 1000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Milligram" && comboBox2.Text == "Kilogram")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin / 1000000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Gram" && comboBox2.Text == "Milligram")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin * 1000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Gram" && comboBox2.Text == "Kilogram")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin / 1000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Kilogram" && comboBox2.Text == "Milligram")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin * 1000000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Kilogram" && comboBox2.Text == "Gram")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin * 1000;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            //TIME CONVERSIONS
+            if (comboBox1.Text == "Seconds" && comboBox2.Text == "Minutes")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin / 60;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Seconds" && comboBox2.Text == "Hours")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin *3600;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Minutes" && comboBox2.Text == "Seconds")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin *60;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Minutes" && comboBox2.Text == "Hours")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin / 60;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Hours" && comboBox2.Text == "Seconds")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin * 3600;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Hours" && comboBox2.Text == "Minutes")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin * 60;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Celsius" && comboBox2.Text == "Fahrenheit")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = (userin * 9/5)+32;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Celsius" && comboBox2.Text == "Kelvins")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin + 273.15;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Fahrenheit" && comboBox2.Text == "Celsius")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = (userin - 32) * 5 / 9;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Fahrenheit" && comboBox2.Text == "Kelvins")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = (userin - 32) * 5 / 9 + 273.15;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Kelvins" && comboBox2.Text == "Celsius")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = userin - 273.15;
+                textBox2.Text = Convert.ToString(userout);
+            }
+            if (comboBox1.Text == "Kelvins" && comboBox2.Text == "Fahrenheit")
+            {
+                userin = Convert.ToDouble(textBox1.Text);
+                userout = (userin - 273.15) * 9/5 + 32;
+                textBox2.Text = Convert.ToString(userout);
+            }
+        }
+    }
+}
